cd069f5c245caecedad15433baa4228351e43232
luisbiset <luis.biset@inema.ba.gov.br>
2025-11-29 13:43:09 -0300

    fix: corrige fluxo de cadastro de paciente com usuário

---
+++ src/main/java/br/gov/ba/sesab/converter/TelefoneConverter.java
@@ -0,0 +1,25 @@
+package br.gov.ba.sesab.converter;
+
+import jakarta.faces.component.UIComponent;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.convert.Converter;
+import jakarta.faces.convert.FacesConverter;
+
+@FacesConverter(forClass = String.class, value = "telefoneConverter")
+public class TelefoneConverter implements Converter<String> {
+
+    @Override
+    public String getAsObject(FacesContext context, UIComponent component, String value) {
+        if (value == null) return null;
+
+        // Remove tudo que não for número
+        return value.replaceAll("\\D", "");
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, String value) {
+        if (value == null) return "";
+
+        return value;
+    }
+}
